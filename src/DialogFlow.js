f617795ab32f30c641c71d5bdec327144de32c61
valentyn.lapotkov <valentyn.lapotkov@globallogic.com>
2018-11-23 15:47:24 +0200

    new Agent

--- src/DialogFlow.js
+++ src/DialogFlow.js
@@ -4,18 +4,15 @@ class DialogFlow extends Component {
     render() {
         return (
             <Fragment>
-                <iframe
-                    allow="microphone;"
-                    width="350"
-                    height="430"
-                    src="https://console.dialogflow.com/api-client/demo/embedded/95d342cb-1985-46e8-92e9-deeedb4d9b00">
-                </iframe>
+            <iframe
+                allow="microphone;"
+                width="350"
+                height="430"
+                src="https://console.dialogflow.com/api-client/demo/embedded/3d0386a1-187a-47b9-8690-8023ff01e194">
+              </iframe>
             </Fragment>
         );
     }
 }
 
 export default DialogFlow;
-
-
-
